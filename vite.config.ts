71ef1dfd5d229fb195a0d9ff5819d3e10b2083c7
a876691666 <928200728@qq.com>
2025-05-13 13:21:16 +0800

    feat: 修正base

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
+  base: "/image-set-hue",
   build: {
-    outDir: 'docs', // 输出目录设置为 docs
+    outDir: "docs", // 输出目录设置为 docs
   },
 });
