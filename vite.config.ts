e755dbe27dd5b031761d7be7f284bd6fa8915ffa
a876691666 <928200728@qq.com>
2025-05-13 13:14:20 +0800

    feat: clear vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,3 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-import dts from 'vite-plugin-dts';
-import { fileURLToPath } from 'url';
+import { defineConfig } from "vite";
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
-
-export default defineConfig({
-	plugins: [
-		dts({
-			outDir: 'dist',
-			insertTypesEntry: true,
-		})
-	],
-	build: {
-		lib: {
-			entry: path.resolve(__dirname, 'src/index.ts'),
-			name: 'ImageSetHue',
-			fileName: (format) => `index.${format}.js`,
-		},
-		outDir: 'docs', // 输出目录为 docs
-		rollupOptions: {
-			external: [], // Specify external dependencies here
-			output: {
-				globals: {}, // Provide global variable names for external dependencies
-			},
-		},
-	},
-});
+export default defineConfig({});
